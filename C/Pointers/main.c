7c58832f4e6f66c2cd81b6b09962a28667591ac5
BRonen <brennopereira6@gmail.com>
2020-11-07 18:27:46 -0300

    pointers as parameters

--- C/Pointers/main.c
+++ C/Pointers/main.c
@@ -1,13 +1,19 @@
 
 #include <stdio.h>
 
-void main () {
+void changeValue(int *ptr){
+    *ptr = (printf("changed\n"), 24);
+}
 
+void main (){
    int  var = 42;
    int  *ptr;
    ptr = &var;
 
    printf("Address of var: %x\n", &var  );
-   printf("Address in ptr: %x\n", ptr );
-   printf("Value of *ptr:  %d\n", *ptr );
+   printf("Address in ptr: %x\n\n", ptr );
+
+   printf("Value of *ptr before:  %d\n", *ptr );
+   changeValue(ptr);
+   printf("Value of *ptr after:  %d\n", *ptr );
 }
