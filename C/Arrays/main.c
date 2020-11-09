363aa381e886081c5b191e43a880672c4974eb1d
Brenno Pereira Rodrigues <brennopereira6@gmail.com>
2020-11-09 10:49:18 -0300

    Pointer Arithmetic

--- C/Arrays/main.c
+++ C/Arrays/main.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
 #include <assert.h>
 
 #define size 4
 
 int main(){
     int arr[size] = {0, 1, 2, 3};
+    int* ptr = arr;
+    assert(*ptr == arr[0]);
+
     for(int i = 0; i <= size; i++){
-        assert(i == arr[i]);
+        assert(i == *ptr);
+	ptr++;
     }
+
     return 0;
 }
