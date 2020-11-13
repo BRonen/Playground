7cec5ff9b0d80f343805f4f7ed4fe0bcc8d306a5
Brenno Pereira Rodrigues <brennopereira6@gmail.com>
2020-11-12 21:31:39 -0300

    added useful Vector's functions

--- C/Vector/Vector.c
+++ C/Vector/Vector.c
@@ -3,13 +3,32 @@
 
 int main(){
     struct Vector vec = vectorCreate();
-    assert(vec.lenght == 0);
+    assert(vec.length == 0);
     assert(vec.capacity == SIZE);
 
-    for(int i = 5; i<100; i+=10){
+    for(int i = 1; i<50; i++){
       vectorPush(&vec, i);
-      assert(vectorPop(&vec) == i);
     }
+
+    for(int i = 1; i<45; i++){
+      assert(vectorPop(&vec) == 50-i);
+    }
+
+    vectorInsert(&vec, 5, 42);
+    assert(vectorAtIndex(&vec, 5) == 42);
+
+    vectorDelete(&vec, 5);
+    assert(vectorAtIndex(&vec, 5) == 0);
+
+    assert(vectorAtIndex(&vec, 3) == 4);
+    vectorRemove(&vec, 4);
+    assert(vectorAtIndex(&vec, 3) == 0);
+
+    vectorPrepend(&vec, 24);
+    assert(vectorAtIndex(&vec, 0) == 24);
+
+    assert(vectorFind(&vec, 54) == -1);
+    assert(vectorFind(&vec, 2) == 2);
 
     vectorDecreate(&vec);
 
--- C/Vector/Vector.h
+++ C/Vector/Vector.h
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define SIZE 4
 
 struct Vector{
   int* data;
-  int lenght;
+  int length;
   int capacity;
   int* ptr;
 };
@@ -15,8 +14,33 @@ struct Vector vectorCreate(){
   vec.data     = malloc(SIZE*sizeof(int));
   vec.ptr      = vec.data;
   vec.capacity = SIZE;
-  vec.lenght   = 0;
+  vec.length   = 0;
   return vec;
+}
+
+int vectorSize(struct Vector* vec){
+  return vec->length;
+}
+
+int vectorCapacity(struct Vector* vec){
+  return vec->capacity;
+}
+
+int vectorIsEmpty(struct Vector* vec){
+  return vec->length == 0;
+}
+
+int vectorAtIndex(struct Vector* vec, int index){
+  return vec->data[index];
+}
+
+void vectorResize(struct Vector* vec, int size){
+  vec->capacity = size;
+  vec->data = (int*)realloc(
+    vec->data, vec->capacity*sizeof(int)
+  );
+  vec->ptr = vec->data;
+  vec->ptr += vec->length;
 }
 
 void vectorDecreate(struct Vector* vec){
@@ -24,25 +48,61 @@ void vectorDecreate(struct Vector* vec){
 }
 
 void vectorPush(struct Vector* vec, int value){
-  if(vec->lenght == vec->capacity){
-    vec->capacity += 10;
-    vec->data = (int*)realloc(
-      vec->data, vec->capacity*sizeof(int)
-    );
+  if(vec->length == vec->capacity){
+    vectorResize(vec, vec->capacity*2);
   }
 
   *vec->ptr = value;
   vec->ptr++;
-  vec->lenght++;
+  vec->length++;
 }
-
 
 int vectorPop(struct Vector* vec){
   vec->ptr--;
   int value = *vec->ptr;
   *vec->ptr = 0;
-  vec->lenght--;
-
+  vec->length--;
+  if(vec->length <= vec->capacity/4){
+    vectorResize(vec, vec->capacity/2);
+  }
   return value;
+}
 
+void vectorDelete(struct Vector* vec, int index){
+  for(int i = index; i < vec->capacity-1; i++){
+    vec->data[i] = vec->data[i+1];
+  }
+  vectorPop(vec);
+}
+
+void vectorInsert(struct Vector* vec, int index, int value){
+  int tmp = vec->data[vec->length-1];
+  for(int i = vec->length; i > index; --i){
+    if(vec->length >= vec->capacity){
+      vectorResize(vec, vec->capacity/2);
+    }
+    vec->data[i] = vec->data[i-1];
+  }
+  vec->data[index] = value;
+}
+
+void vectorPrepend(struct Vector* vec, int value){
+  vectorInsert(vec, 0, value);
+}
+
+int vectorFind(struct Vector* vec, int value){
+  for(int i = 1; i < vec->length; i++){
+    if(vectorAtIndex(vec, i) == value){
+      return i;
+    }
+  }
+  return -1;
+}
+
+void vectorRemove(struct Vector* vec, int value){
+  for(int i = 1; i < vec->length; i++){
+    if(vectorAtIndex(vec, i) == value){
+      vectorDelete(vec, i);
+    }
+  }
 }
